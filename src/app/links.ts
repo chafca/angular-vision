12a6f55a0bea240e68963a6ca6d11eb869baefb1
hamoud manlid <chafca@users.noreply.github.com>
2017-05-11 14:30:11 +0200

    Update links.ts

--- src/app/links.ts
+++ src/app/links.ts
@@ -1,3 +1,3 @@
 'use strict';
 
-export const Url = 'URL';
+export const Url = 'http://recg-ws.vision.services.local/cxf/rest/pdc/search/Commande/{"bool":{"must":%5B{"prefix":{"numerocommandeunique":"CCL"}}%5D,"must_not":%5B{"term":{"numeroannonceur":"00000006"}}%5D}}';
